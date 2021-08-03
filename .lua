4bf6db749fcbbc497cbc38648b38a05d8885042c
compwnter <64485899+compwnter@users.noreply.github.com>
2021-08-03 13:00:47 +0200

    Update .lua

--- .lua
+++ .lua
@@ -1,5 +1,5 @@
 coroutine.resume(coroutine.create(function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Legacy-Foundation/Legacy-Hub/main/.res/.src'))();
 end));
-wait();
+wait(1);
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Legacy-Foundation/Legacy-Hub/main/.res/.xxl'))();
