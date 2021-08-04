5305a4c2c668dcc4be2e4ce1e8dcb992be154c11
compwnter <64485899+compwnter@users.noreply.github.com>
2021-08-04 09:21:39 +0200

    Update .lua

--- .lua
+++ .lua
@@ -1,12 +1,1 @@
-
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Legacy-Foundation/Legacy-Hub/main/.res/.src'))();
-
-wait(1);
-pcall(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Legacy-Foundation/Legacy-Hub/main/.res/.xxl'))();
-end);
-while wait(3) do
-    pcall(function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/Legacy-Foundation/Legacy-Hub/main/.res/.xxl'))();
-    end);
-end);
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Legacy-Foundation/Legacy-Hub/main/.res/.src'))();
